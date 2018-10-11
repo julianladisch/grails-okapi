1d228615c96ae25ff176ffd1f9c3da73f7ca3a92
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-10-11 13:03:05 +0100

    Pulled in latest toolkit and rejigged controllers to match,

--- src/main/groovy/com/k_int/okapi/OkapiRestfulController.groovy
+++ src/main/groovy/com/k_int/okapi/OkapiRestfulController.groovy
@@ -1,16 +1,12 @@
 package com.k_int.okapi
 
-import com.k_int.web.tools.SimpleLookupService
-
+import com.k_int.web.toolkit.rest.RestfulController
 import grails.artefact.Artefact
-import grails.rest.RestfulController
 
 @Artefact('Controller')
 class OkapiRestfulController<T> extends RestfulController<T> {
 
   static responseFormats = ['json', 'xml']
-  SimpleLookupService simpleLookupService
-
   OkapiRestfulController(Class<T> resource) {
     super(resource)
   }
@@ -24,18 +20,6 @@ class OkapiRestfulController<T> extends RestfulController<T> {
   }
   
   def index() {
-    
-    final int offset = params.int("offset") ?: 0
-    final int perPage = Math.min(params.int('perPage') ?: params.int('max') ?: 100, 100)
-    final int page = params.int("page") ?: (offset ? (offset / perPage) + 1 : 1)
-    final List<String> filters = params.list("filters[]") ?: params.list("filters")
-    final List<String> match_in = params.list("match[]") ?: params.list("match")
-    final List<String> sorts = params.list("sort[]") ?: params.list("sort")
-    
-    if (params.boolean('stats')) {
-      respond simpleLookupService.lookupWithStats(this.resource, params.term, perPage, page , filters, match_in, sorts)
-    } else {
-      respond simpleLookupService.lookup(this.resource, params.term, perPage, page , filters, match_in, sorts)
-    }
+    respond doTheLookup()
   }
 }
--- src/main/groovy/com/k_int/okapi/OkapiTenantAwareController.groovy
+++ src/main/groovy/com/k_int/okapi/OkapiTenantAwareController.groovy
@@ -1,12 +1,9 @@
 package com.k_int.okapi
 
-import java.util.List
-
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.access.AccessDeniedHandlerImpl
 
 import com.k_int.web.toolkit.rest.TenantAwareRestfulController
-import com.k_int.web.tools.SimpleLookupService
 
 import grails.artefact.Artefact
 import grails.gorm.multitenancy.CurrentTenant
@@ -18,7 +15,6 @@ class OkapiTenantAwareController<T> extends TenantAwareRestfulController<T> {
 
   static responseFormats = ['json', 'xml']
   
-  SimpleLookupService simpleLookupService
   SpringSecurityService springSecurityService
 
   OkapiTenantAwareController(Class<T> resource) {
@@ -47,21 +43,5 @@ class OkapiTenantAwareController<T> extends TenantAwareRestfulController<T> {
   protected boolean hasAuthority(String auth) {
     def intersect = hasAnyAuthority((auth ? [auth] : []) as Set)
     intersect
-  }
-  
-  def index() {
-    
-    final int offset = params.int("offset") ?: 0
-    final int perPage = Math.min(params.int('perPage') ?: params.int('max') ?: 100, 100)
-    final int page = params.int("page") ?: (offset ? (offset / perPage) + 1 : 1)
-    final List<String> filters = params.list("filters[]") ?: params.list("filters")
-    final List<String> match_in = params.list("match[]") ?: params.list("match")
-    final List<String> sorts = params.list("sort[]") ?: params.list("sort")
-    
-    if (params.boolean('stats')) {
-      respond simpleLookupService.lookupWithStats(this.resource, params.term, perPage, page, filters, match_in, sorts)
-    } else {
-      respond simpleLookupService.lookup(this.resource, params.term, perPage, page, filters, match_in, sorts)
-    }
   }
 }

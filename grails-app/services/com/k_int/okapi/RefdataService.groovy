f01bab8386b5d3d2f59ff4b0bc3f9f13abc23e49
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-11-28 17:55:57 +0000

    Add the refdataService to the module.

---
+++ grails-app/services/com/k_int/okapi/RefdataService.groovy
@@ -0,0 +1,18 @@
+package com.k_int.okapi;
+
+import grails.events.Event
+import grails.events.annotation.Subscriber
+import groovy.util.logging.Slf4j
+import com.k_int.web.toolkit.refdata.GrailsDomainRefdataHelpers
+
+@Slf4j
+public class RefdataService {
+    
+  @Subscriber('okapi:tenant_schema_created')
+  public void onTenantSchemaCreated(String new_schema_name) {
+    
+    log.debug("RefdataService::onTenantSchemaCreated(${new_schema_name})")
+    // Skip this until we can work out whats going wrong...
+    GrailsDomainRefdataHelpers.setDefaultsForTenant(new_schema_name)
+  }
+}

4d7e4bdd6c26b0cbd31f2b1bfe0d66fb4e1f9777
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2021-05-11 15:21:01 +0100

    chore: Remove debug using System.out

--- grails-app/controllers/com/k_int/okapi/OkapiMdcInterceptor.groovy
+++ grails-app/controllers/com/k_int/okapi/OkapiMdcInterceptor.groovy
@@ -1,8 +1,7 @@
 package com.k_int.okapi
 
-import javax.servlet.http.HttpServletRequest
-import org.slf4j.MDC
 import groovy.transform.CompileStatic
+import org.slf4j.MDC
 
 
 @CompileStatic
@@ -10,36 +9,31 @@ public class OkapiMdcInterceptor {
   
   int order = HIGHEST_PRECEDENCE + 50
 
-  private final static String PREFIX = "x-okapi-"
-  private static final String[] headersToVars = [
-    OkapiHeaders.TENANT,
-  ] as String[]
+  private final static String PREFIX = 'x-okapi-'
+  
+  private static final String[] headersToVars = [OkapiHeaders.TENANT] as String[]
+  
 
   public OkapiMdcInterceptor() {
-    log.debug "OkapiMdcInterceptor::Init"
     matchAll()
   }
 
   boolean before() {
     for (final String var : headersToVars) {
       
-      log.debug "Looking for ${var}"
-      String val = request.getHeader(var)?.trim()
+      final String val = request.getHeader(var)?.trim()
       if (val) {
         
         String key = var.toLowerCase()
-        
         key = key.startsWith(PREFIX) ? key.substring(PREFIX.length()) : key
-        
-        log.debug "Adding log val ${key}"
-        MDC.put(key, val)
+        MDC.put("{$key}", "${val}")
       }
     }
 
     true
   }
 
-  void afterView() {
+  void afterView() {    
     MDC.clear()
   }
 }
